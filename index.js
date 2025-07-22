5707fe32caf037d6f6bb9ba90e0cd5a5b76d527a
jalvarenga11 <juan.alvarenga@ess.hn>
2025-07-22 17:47:41 -0600

    Agregando mecanismos de seguridad

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ import cors from 'cors' // disponible gracias a la instalación
 import authRoutes from './routes/auth.routes.js' // rutas de autenticación
 import moviesRoutes from './routes/movies.routes.js'
 import dotenv from 'dotenv' // para cargar las variables de entorno desde el archivo .env
+import helmet from "helmet";
+import { rateLimit } from 'express-rate-limit'
+import { success } from 'zod/v4'
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    limit: 50,
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    message: {
+        success: false,
+        message: 'Too many requests, please try again later.'
+    }
+})
+
 
 const app = express() // para crear la aplicación de express
 
 dotenv.config()// cargar las varianles del entorno desde el archivo .env
-
+app.use(helmet()); // para mejorar la seguridad de la aplicación
+app.use(limiter)
 const PORT = process.env.PORT || 3000 // puerto donde se ejecutará la aplicación
 
 // middlewares
